0ada67a04eee454d3f6a078181f2d45899f6527e
henriquecechinel <henrique-cechinel@hotmail.com>
2021-08-09 01:33:51 -0300

    Continuidade na tela Birthday

--- app/controllers/birthdayController.js
+++ app/controllers/birthdayController.js
@@ -1,5 +1,39 @@
 app.controller('BirthdayController', function($scope) {
+  var vm = $scope;
 
-    $scope.message = 'Hello from BirthdayController';
+  const codigosAcesso = new Map();
+  codigosAcesso.set('H', "Henrique e Maria Luiza");
+  codigosAcesso.set('FEINE-BLS', "Felipe e Thaine");
+  codigosAcesso.set('HENARA-CURITIBA', "Henrique e Nara");
+
+  vm.codigoAcesso = "";
+  vm.zonaAcesso = 0;
+  vm.convidados = "";
+
+  vm.setZonaAcesso = function(zona){
+    console.log(zona);
+    vm.zonaAcesso = zona;
+  };
+
+  vm.updateConvidados = function(){
+    vm.convidados = codigosAcesso.get(vm.codigoAcesso.toUpperCase())
+  };
+
+  vm.verificaAcesso = function() {
+    vm.updateConvidados();
+
+    if(vm.convidados){
+      swal({
+        title: 'Uhuuuuuuuuuulll!',
+        text: 'Você foi convidado para o super aniversário da Maria Luiza!',
+        icon: "success",
+        timer: 6000,
+        buttons: false
+      });
+      vm.setZonaAcesso(1);
+    } else {
+      vm.setZonaAcesso(0);
+    }
+  };
 
 });
