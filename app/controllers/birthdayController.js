89c81fefc024abb0d14edd9126325674c944a9b2
henriquecechinel <henrique-cechinel@hotmail.com>
2021-08-10 01:40:19 -0300

    Continuação do desenvolvimento do projeto Birthday

--- app/controllers/birthdayController.js
+++ app/controllers/birthdayController.js
@@ -7,32 +7,96 @@ app.controller('BirthdayController', function($scope) {
   codigosAcesso.set('HENARA-CURITIBA', "Henrique e Nara");
 
   vm.codigoAcesso = "";
-  vm.zonaAcesso = 0;
+  vm.accessZone = 0;
   vm.convidados = "";
 
-  vm.setZonaAcesso = function(zona){
-    console.log(zona);
-    vm.zonaAcesso = zona;
+  swal("Sound on!", "Aumente o som!", "info", {buttons: false, timer: 4000});
+
+  setAccessZone = function(zone){
+    console.log(zone);
+    vm.accessZone = zone;
   };
 
-  vm.updateConvidados = function(){
+  setVideoNumber = function(number){
+    vm.videoNumber = number;
+  }
+
+  refreshGuests = function(){
     vm.convidados = codigosAcesso.get(vm.codigoAcesso.toUpperCase())
   };
 
+  audioOhanaMeansFamily = {
+    start: function(){
+      $('#ohanaMeansFamily').trigger("play");
+      ohanaInterval = setInterval(function(){$('#ohanaMeansFamily').trigger("play")},13000);
+    },
+
+    pause: function(){
+      $('#ohanaMeansFamily').trigger("pause");
+      clearInterval(ohanaInterval);
+    }
+  };
+
   vm.verificaAcesso = function() {
-    vm.updateConvidados();
+    refreshGuests();
 
     if(vm.convidados){
+      audioOhanaMeansFamily.start();
+
       swal({
-        title: 'Uhuuuuuuuuuulll!',
-        text: 'Você foi convidado para o super aniversário da Maria Luiza!',
+        title: 'Ohana!',
+        text: 'Você foi convidado para o super aniversário da Luiza!',
         icon: "success",
-        timer: 6000,
-        buttons: false
-      });
-      vm.setZonaAcesso(1);
+        button: "Ohana!"
+      })
+      .then((value) => {
+        audioOhanaMeansFamily.pause();
+
+        $scope.$apply(function(){
+          setAccessZone(1);
+          setVideoNumber(0);
+        })
+
+        setTimeout(function(){$('#introStitch').trigger("play")}, 1000); 
+
+        document.getElementById('introStitch').addEventListener('ended', introStitchHandler, false);
+        function introStitchHandler(e) {
+          document.getElementById('introStitch').removeEventListener('ended', introStitchHandler, false);
+
+          $scope.$apply(function(){
+            console.log("teste");
+            setAccessZone(2);
+          })
+        }
+      })
+
     } else {
-      vm.setZonaAcesso(0);
+      setAccessZone(0);
+    }
+  };
+
+  vm.togglePresence = function(videoNumber){
+    setAccessZone(1);
+    setVideoNumber(videoNumber);
+
+    switch(videoNumber){
+      case 1:
+        id = 'sadStitch';
+        break;
+      case 2:
+        id = 'happyStitch';
+        break;
+    }
+
+    setTimeout(function(){$('#'+id).trigger("play")}, 1000); 
+
+    document.getElementById(id).addEventListener('ended', dynamicStitchHandler, false);
+    function dynamicStitchHandler(e) {
+      document.getElementById(id).removeEventListener('ended', dynamicStitchHandler, false);
+
+      $scope.$apply(function(){
+        setAccessZone(2);
+      })
     }
   };
 
